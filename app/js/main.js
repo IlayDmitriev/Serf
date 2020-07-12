92342532f41d7f4cc814a14eb1a1e3ebd44fef10
Ilay <id099663@gmail.com>
2020-07-12 19:21:40 +0300

    section serf + section travel

--- app/js/main.js
+++ app/js/main.js
@@ -5,12 +5,26 @@ $(function(){
       fade: true,
       prevArrow: ' <img class="slider-arrows slider-arrows__left" src="img/left.svg" alt="right">',
       nextArrow: ' <img class="slider-arrows slider-arrows__right" src="img/right.svg" alt="right">',
-      asNavFor: '.slider-dots',
+      asNavFor: '.slider-dotshead',
  });
- $('.slider-dots').slick({
+ $('.slider-dotshead').slick({
     slidesToShow: 4,
     slidesToScroll: 4,
     asNavFor: '.header__slider',
+ });
+ $('.serf__slider').slick({
+   slidesToShow: 4,
+   slidesToScroll: 1,
+   prevArrow: ' <img class="slider-arrows slider-arrows__left" src="img/left.svg" alt="right">',
+   nextArrow: ' <img class="slider-arrows slider-arrows__right" src="img/right.svg" alt="right">',
+   asNavFor: '.slider-map',
+ });
+ $('.slider-map').slick({
+   slidesToShow: 8,
+   slidesToScroll: 1,
+   arrows: false,
+   asNavFor: '.serf__slider',
+   focusOnSelect: true
  });
     
 });
