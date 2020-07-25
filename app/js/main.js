d511d0ea93d0e684f95c41ef6b4c4e0164a344a2
Ilay <id099663@gmail.com>
2020-07-25 13:56:03 +0300

    surfboard + footer

--- app/js/main.js
+++ app/js/main.js
@@ -26,5 +26,55 @@ $(function(){
    asNavFor: '.serf__slider',
    focusOnSelect: true
  });
+  $('.holder__slider, .shop__slider').slick({
+      infinite: true,
+      fade: true,
+      prevArrow: ' <img class="slider-arrows slider-arrows__left" src="img/left.svg" alt="right">',
+      nextArrow: ' <img class="slider-arrows slider-arrows__right" src="img/right.svg" alt="right">',
+ });
+
+ jQuery('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="img/plus.svg" alt=""></div><div class="quantity-button quantity-down"><img src="img/minus.svg" alt=""></div></div>').insertAfter('.quantity input');
+ jQuery('.quantity').each(function() {
+   var spinner = jQuery(this),
+     input = spinner.find('input[type="number"]'),
+     btnUp = spinner.find('.quantity-up'),
+     btnDown = spinner.find('.quantity-down'),
+     min = input.attr('min'),
+     max = input.attr('max');
+
+   btnUp.click(function() {
+     var oldValue = parseFloat(input.val());
+     if (oldValue >= max) {
+       var newVal = oldValue;
+     } else {
+       var newVal = oldValue + 1;
+     }
+     spinner.find("input").val(newVal);
+     spinner.find("input").trigger("change");
+   });
+
+   btnDown.click(function() {
+     var oldValue = parseFloat(input.val());
+     if (oldValue <= min) {
+       var newVal = oldValue;
+     } else {
+       var newVal = oldValue - 1;
+     }
+     spinner.find("input").val(newVal);
+     spinner.find("input").trigger("change");
+   });
+
+ });
+  $('.quantity-button').on('click', function(){
+    var summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() - 1) * $('.summ').data('guests');
+    $('.summ').html('$' + summ);
+  });
+  let summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() - 1) * $('.summ').data('guests');
+  $('.summ').html('$' + summ);
+ 
+  $('.surfboar-box__circle').on('click', function(){
+    $(this).toggleClass('active')      
+  });
     
 });
+
